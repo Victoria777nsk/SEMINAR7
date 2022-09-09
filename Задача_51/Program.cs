ff8c4f19ee78d38a1583f1aacb86e9cd75457cf6
Victoria <victoriaerygina@yandex.ru>
2022-09-09 13:32:46 +0700

    Сумма элементов по диагонали

---
+++ Задача_51/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.).
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[rows, columns];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1,10);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+int sum = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) sum = sum + array[i, j];
+        }
+    }
+Console.WriteLine($"Сумма элементов, находящихся по диагонали = {sum}");
