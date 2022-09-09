f0541efca637c9d89e6d2bce94ae29123bfca696
Victoria <victoriaerygina@yandex.ru>
2022-09-09 12:28:07 +0700

    Таблица Пифагора (умножения)

---
+++ Задача_48/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₙₙ = m + n.
+// Выведите полученный массив на экран (таблица умножения, таблица Пифагора).
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] MultiplicationTable()
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = i + j;
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+MultiplicationTable();
