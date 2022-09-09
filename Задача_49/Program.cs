9fdec154274cc8b08b5981cf37edfd549f6a131c
Victoria <victoriaerygina@yandex.ru>
2022-09-09 13:15:11 +0700

    Замена элементов с четными индексами

---
+++ Задача_49/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] ElementsReplacement()
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1,10);
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (i % 2 == 0 && j % 2 == 0)
+                {
+                    array[i, j] *= array[i,j];
+                }
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    return array;
+}
+
+ElementsReplacement();
